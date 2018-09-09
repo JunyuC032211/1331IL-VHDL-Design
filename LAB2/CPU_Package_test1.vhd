b0bdb3038937669a175d9a810422a4583716ff00
Roderick Karlemstrand <roderick@karlemstrand.com>
2018-09-09 16:57:30 +0200

    Done add overflow

--- LAB2/CPU_Package_test1.vhd
+++ LAB2/CPU_Package_test1.vhd
@@ -1,27 +1,43 @@
-library ieee;
+Library IEEE;
+Use IEEE.STD_LOGIC_ARITH.ALL;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
 
-package CPU_package is
+Package cpu_package is
+  Constant adress_size: integer := 4;
+  Constant data_size: integer := 4;
+  Constant operation_size: integer := 4;
+  Constant instruction_size: integer := 10;
 
-constant adress_size: integer := 4;
-constant data_size: integer := 4;
-constant operation_size: integer := 4;
-constant instruction_size: integer := 10; 
+  Subtype data_word is std_logic_vector(3 downto 0);
+  Subtype address_bus is std_logic_vector(3 downto 0);
+  Subtype data_bus is std_logic_vector(3 downto 0);
+  Subtype instruction_bus is std_logic_vector(9 downto 0);
+  Subtype program_word is std_logic_vector(9 downto 0);
+  Subtype command_word is std_logic_vector(3 downto 0);
 
-subtype data_word is std_logic_vector (3 downto 0);
-subtype adress_bus is std_logic_vector (3 downto 0);
-subtype data_bus is std_logic_vector (3 downto 0);
-subtype instruction_bus is std_logic_vector (9 downto 0);
-subtype program_word is std_logic_vector (9 downto 0);
-subtype command_word is std_logic_vector (3 downto 0);
+  Function add_overflow(a, b: std_logic_vector) Return std_logic_vector;
+  Function sub_overflow(a, b: std_logic_vector) Return std_logic_vector;
 
-Function add_overflow(a,b : std_logic_vector)
-							return std_logic_vector;
+End;
 
-Function sub_overflow(a,b : std_logic_vector)
-							return std_logic_vector;
+Package Body cpu_package is
+  Function add_overflow(
+    a, b: std_logic_vector
+  )
+  -- carry out is the most sig. bit
+  Return std_logic_vector is
+    Variable in_A: unsigned;
+    Variable in_B: unsigned;
+    Variable out_A: std_logic_vector;
+    Variable sum : unsigned(in_A'length downto 0);
 
+    Begin
+      in_A := unsigned(a);
+      in_B := unsigned(b);
+      sum := ('0' & in_A) + in_B;
+      out_A := std_logic_vector(unsigned(sum));
+      Return out_A;
+    End;
 
-end package CPU_package;
-
+End Package Body;
